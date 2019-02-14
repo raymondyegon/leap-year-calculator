e7695a9bd083e484f8b53034dda533fb3e99ae08
Raymond Yegon <raymondyegon17@gmail.com>
2019-02-14 15:46:35 +0300

    Function of calculating leap year added and connected with webpage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-var 
+function leapYear() {
+  var x ;
+  x = document.getElementById("year").value;
+  if ((0 == x%4) && (0 !== x % 100) || (0 == x % 400) ) {
+    alert ( x + " is a leap year")
+  }
+  else {
+    alert(x + " is not a leap year")
+  }
+}
